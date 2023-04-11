614c6491a84b6d6bf510ae2c6d97fbc7b2249b1e
0RaMsY0 <0ramsy0@gmail.com>
2023-04-11 01:11:00 +0000

    Added a configues reader

---
+++ MusBleep/api/utils/read_conf.py
@@ -0,0 +1,13 @@
+import os
+import json
+
+def conf() -> dict:
+    """
+        This function reads a JSON configuration
+        file located at `CONF_FILE_PATH` and returns
+        its contents as a Python dictionary
+    """
+    CONF_FILE_PATH = "conf/api-conf.json"
+    CONF_FILE_CONTENT = json.loads(open(CONF_FILE_PATH, "r").read())
+
+    return CONF_FILE_CONTENT
