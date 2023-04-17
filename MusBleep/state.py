bc484b55489be0312eaf36368d750c9c7cd03182
0RaMsY0 <0ramsy0@gmail.com>
2023-04-17 05:44:22 +0000

    Changed the upload functionality

--- MusBleep/state.py
+++ MusBleep/state.py
@@ -44,39 +44,42 @@ class MusBleepState(pc.State):
             return
 
         API_URL = conf()["api"]
+        MUSIC_NAME = file.filename if ' ' not in file.filename else file.filename.replace(' ', '_')
+        SAVE_MUSIC_PATH = f"MusBleep/api/cache/{MUSIC_NAME}"
         
-        MUSIC_CONTENT = await file.read()
-        MUSIC_NAME = file.filename
-
-        self.music_name = MUSIC_NAME
-
-        print(MUSIC_NAME)
-        FILE = {
-            "file": MUSIC_CONTENT
-        }
-        PARAMS = {
-            "music_name": MUSIC_NAME
-        }
-
-        RESPONCE = requests.post(
-            f"{API_URL}/api/v1/bleep_music",
-            files=FILE,
-            params=PARAMS
-        )
-        
-        if RESPONCE.status_code == 400:
+        try:
+           with open(SAVE_MUSIC_PATH, "wb") as save_music:
+               while music_content := file.file.read(1024 * 1024):
+                   save_music.write(music_content)
+        except Exception as exception:
             self.is_music_upload_exception_raised = True
             self.ending_message = ERROR_MESSAGES[0]
+            self.is_upload_finish  = True
+            self.show_ending_message = True
+            self.is_upload_started = False
+            self.message_status = MESSAGES_STATUS[self.ending_message]
+            return
+        finally:
+            file.file.close()
         
-        elif RESPONCE.status_code == 409:
+        self.music_name = MUSIC_NAME if len(MUSIC_NAME) < 17 else f"{MUSIC_NAME[:17]} ..."
+
+        PARAMS = {
+            "music_name": f"cache/{MUSIC_NAME}"
+        }
+
+        RESPONCE = requests.get(
+            f"{API_URL}/api/v1/bleep_music",
+            params=PARAMS
+        )
+
+        if RESPONCE.status_code == 409:
             self.is_faild_to_bleep_music_exception_raised = True
             self.ending_message = ERROR_MESSAGES[1]
         
-        elif RESPONCE.status_code == 201:
+        elif RESPONCE.status_code == 200:
             self.bleeped_music_url = f"{API_URL}{RESPONCE.json()['bleeped_music_path']}"
             self.ending_message = SUCCESS_MESSAGES[0]
-
-        print(f"{RESPONCE.json() = }")
 
         self.is_upload_finish  = True
         self.show_ending_message = True
